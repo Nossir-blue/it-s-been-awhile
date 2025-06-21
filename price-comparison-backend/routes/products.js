35440a32f6497ca3e2cc11137e3db2def5447e6b
Nossir-blue <rogerreis20@protonmail.com>
2025-06-21 21:51:07 +0100

    forgot to install with npm, i'm used to fedora already

--- price-comparison-backend/routes/products.js
+++ price-comparison-backend/routes/products.js
@@ -8,19 +8,19 @@ const {
   getStats
 } = require('../controllers/productController');
 
-// Rota para buscar produtos
+
 router.get('/search', searchProducts);
 
-// Rota para comparar preços de um produto específico
+
 router.get('/compare/:productName', compareProductPrices);
 
-// Rota para obter todas as lojas
+
 router.get('/stores', getStores);
 
-// Rota para obter todas as categorias
+
 router.get('/categories', getCategories);
 
-// Rota para obter estatísticas gerais
+
 router.get('/stats', getStats);
 
 module.exports = router;
